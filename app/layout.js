16222eaacf2f2cb9bef9fdc299d0f288920a820e
mainavinhoon <navinrawat11032004@gmail.com>
2023-10-30 23:42:18 +0530

    Navbar is made responsive, hamburger menu is added for mobile view

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className=" sm:mt-0 sm:m-8 ">
         <Navbar/>
         {children}
         <Footer/>
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,86 +1,105 @@
 "use client"
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Events from '../events/Events'
-import AboutUs from '@/app/AboutUs/page'
-import { useState } from 'react'
-import Home from '@/app/page'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { FaBars , FaUser } from 'react-icons/fa'; // Import the menu icon from react-icons/fa
+
 
 const Navbar = () => {
-  const menus=[
-   { label:"Home" ,value: "/"},
-    {label:"AboutUS" ,value: "/AboutUs"},
-   { label:"Events" ,value: "/#Events"},
- {   label:"Community" ,value: "/Community"},
-   { label:"Maps" ,value: "/#"},
-    
-  ]
+  const menus = [
+    { label: 'Home', value: '/' },
+    { label: 'AboutUS', value: '/AboutUs' },
+    { label: 'Events', value: '/#Events' },
+    { label: 'Community', value: '/Community' },
+    { label: 'Maps', value: '/#' },
+  ];
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [currTab,setCurrTab]=useState("Home")
+  const [currTab, setCurrTab] = useState('Home');
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+
 
   const toggleLoginStatus = () => {
     setIsLoggedIn(!isLoggedIn);
   };
+  const toggleProfileMenu = () => {
+    setProfileMenuOpen(!isProfileMenuOpen);
+  };
+
   return (
-    <nav className=' flex py-4 px-6 items-center '>
-
-      <div >
-        {/* <Image className=' ' src="logo.png" alt='' width={50} height={50}/> */}
-        <img src="https://www.giftgujarat.in/assets/common/vectors/logo-dark.svg" className='' width={50} height={50} alt="" />
+    <nav className="flex py-4 px-6  items-center">
+      <div>
+        <img
+          src="https://www.giftgujarat.in/assets/common/vectors/logo-dark.svg"
+          className=""
+          width={50}
+          height={50}
+          alt=""
+        />
       </div>
-      <div className=' flex justify-evenly  gap-x-5 ml-auto '>
-
-          {/* <Link href="/">
-              Home
-          </Link>
-          <Link href="/AboutUs">
-              About Us
-          </Link>
-          <Link href="/#Events">
-                Events
-          </Link>
-          <Link href="/Community">
-              Community
-          </Link>
-          <Link href="/#">
-              Map
-          </Link> */}
-          {menus.map((item,index)=>(
-          <Link 
-          href={item.value}
-          key={index}
-          className={`${currTab ==item.label ?"border-b-2  border-gray-600" : "" }`}
-          onClick={()=>setCurrTab(item.label)}
+      {/* Mobile menu button */}
+      <div className="ml-auto block sm:hidden">
+        <button
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+          className="text-gray-600 focus:outline-none"
+        >
+         <FaBars />
+        </button>
+      </div>
+      {/* Desktop navigation */}
+      <div className="hidden sm:flex justify-evenly gap-x-5 ml-auto">
+        {menus.map((item, index) => (
+          <Link
+            href={item.value}
+            key={index}
+            className={`${currTab === item.label ? 'border-b-2 border-gray-600' : ''}`}
+            onClick={() => setCurrTab(item.label)}
           >
             {item.label}
           </Link>
-
+        ))}
+      </div>
+      {/* Mobile menu */}
+      <div className={`ml-auto sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
+        <div className="absolute top-16 right-0 left-0 bg-white p-4 border-b-2 border-gray-600">
+          {menus.map((item, index) => (
+            <Link
+              href={item.value}
+              key={index}
+              className="block py-2 text-gray-600"
+              onClick={() => {
+                setMobileMenuOpen(false);
+                setCurrTab(item.label);
+              }}
+            >
+              {item.label}
+            </Link>
           ))}
-          </div>
-          <div className=' ml-16 font-medium text-lg cursor-pointer text-black'>
-          {/* Add more links as needed */}
-          <button onClick={!isLoggedIn ? toggleLoginStatus : null}>
-            {isLoggedIn ? (
-              <Link href="/Profile">Profile</Link>
-            ) : (
-              <Link href="/Login">Login</Link>
-            )}
+        </div>
+      </div>
+      <div className="ml-4 font-medium text-lg cursor-pointer text-black">
+        {isLoggedIn ? (
+          <button onClick={toggleProfileMenu}>
+            <FaUser />
           </button>
-          {isLoggedIn && (
-            <ul className="">
-              <li onClick={isLoggedIn ? toggleLoginStatus : null}>
-                <Link href="/">Logout</Link>
-              </li>
-            </ul>
-              )}
-
-         </div>
-     
-     
+        ) : (
+          <button onClick={toggleLoginStatus}>
+            <Link href="/Login">Login</Link>
+          </button>
+        )}
+        {isProfileMenuOpen && (
+          <ul className="bg-white p-2 border-b-2 border-gray-600 absolute right-0 mt-2">
+            <li>
+              <Link href="/Profile">Profile</Link>
+            </li>
+            <li onClick={ toggleProfileMenu }>
+              <Link onClick={toggleLoginStatus } href="/">Logout</Link>
+            </li>
+          </ul>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
